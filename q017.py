597d3d9fe635fa9cd840f60a2b52cbb0b68fb7fb
2swap <mchontz10@gmail.com>
2019-08-01 14:18:54 -0400

    17

---
+++ q017.py
@@ -0,0 +1,9 @@
+#one through nine:				36
+#ten through twenty:				70
+#20, 30, ..., 90:				460
+#1-99:						854
+#one hundred, two hundred, ..., nine hundred:	99
+#All the "and"s:				2673
+#One Thousand:					11
+
+#Answer: 99*100 + 854*10 + 2673 + 11 = 21124
